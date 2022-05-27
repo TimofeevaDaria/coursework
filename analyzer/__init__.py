e51766f3f27703d1333191a7bd6f0ee45957e7ab
timofeevadaria <80330784+timofeevadaria@users.noreply.github.com>
2022-05-27 19:34:46 +0300

    Add files via upload

---
+++ analyzer/__init__.py
@@ -0,0 +1,18 @@
+"""
+Сервис с REST API, анализирующий рынок для промо-акций.
+Пример решения вступительного испытания 2019г. в Школу бэкенд‑разработки
+Яндекса.
+"""
+__author__ = 'Timofeeva Daria'
+__maintainer__ = __author__
+
+__email__ = 'timofeeva1801@yandex.ru'
+__version__ = '0.0.1'
+
+
+__all__ = (
+    '__author__',
+    '__email__',
+    '__maintainer__',
+    '__version__',
+)
